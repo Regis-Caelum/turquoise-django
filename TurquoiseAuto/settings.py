f65d36fc5d354e8f85783bcb56a3e8122b001bbf
Regis-Caelum <khanmf@rknec.edu>
2022-05-23 00:13:18 +0530

    Update DB creds

--- TurquoiseAuto/settings.py
+++ TurquoiseAuto/settings.py
@@ -11,11 +11,10 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
-import django_heroku
+import django_heroku, os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
@@ -27,7 +26,6 @@ SECRET_KEY = 'django-insecure-g%)pr74zy*(1_)bg1xhhq4#ihc^bn896vi&cp+ew86(7!k)_!k
 DEBUG = True
 
 ALLOWED_HOSTS = []
-
 
 # Application definition
 
@@ -72,17 +70,22 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'TurquoiseAuto.wsgi.application'
 
-
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'HOST': 'ec2-3-229-161-70.compute-1.amazonaws.com',
+        'NAME': 'd55ou67tum4e3m',
+        'USER': 'hhftkvekdinyvy',
+        'PASSWORD': 'b40ad96e30d8bc239cc8084e13c76cd423aa3beb135e15633e96fe2bdd84c718'
+        # 'HOST': os.environ.get('DATABASE_URL'),
+        # 'DATABASE': os.environ.get('DATABASE'),
+        # 'USER': os.environ.get('USER'),
+        # 'PASSWORD': os.environ.get('PASSWORD'),
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
@@ -102,7 +105,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/4.0/topics/i18n/
 
@@ -113,7 +115,6 @@ TIME_ZONE = 'UTC'
 USE_I18N = True
 
 USE_TZ = True
-
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
